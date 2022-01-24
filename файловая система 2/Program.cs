9445dc54b18a94dd5a0fc238a4deb3da9f7d9804
Юлия <@LAPTOP-B0K7S1UE>
2022-01-24 20:11:22 +0700

    Добавьте файлы проекта.

---
+++ файловая система 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace файловая_система_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Begin:
+            Console.Write("Введите полное имя файла: C:\\");
+            string username = "C:\\" + Console.ReadLine();
+            Random x = new Random();
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(username, false, Encoding.Default))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        double q = x.Next(-1000, 1000);
+                        sw.WriteLine(q);
+                    }
+                }
+                double[] mas = new double[10];
+                using (StreamReader sr = new StreamReader(username, Encoding.Default))
+                {
+                    double S = 0;
+                    int i = 0;
+                    while (i < 10)
+                    {
+                        S += Convert.ToDouble(sr.ReadLine());
+                        i++;
+                    }
+                    Console.WriteLine($"Сумма чисел в файле = {S}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                goto Begin;
+            }
+            Console.ReadLine();
+        }
+    }
+}
